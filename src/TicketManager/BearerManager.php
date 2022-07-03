b9bda064264a478bb7dba9835ed90cc16c2a150f
Макс Желтиков <zheltikovma@mos-team.ru>
2022-07-03 14:00:46 +0300

    Added bearer token gathering

--- src/TicketManager/BearerManager.php
+++ src/TicketManager/BearerManager.php
@@ -2,15 +2,32 @@
 
 namespace Zheltikov\Auth\TicketManager;
 
+use Psr\Http\Message\RequestInterface;
 use Zheltikov\Auth\Ticket\Token;
 use Zheltikov\Auth\TicketInterface;
 use Zheltikov\Auth\TicketManagerInterface;
 
 abstract class BearerManager implements TicketManagerInterface
 {
+    protected RequestInterface $request;
+
     public function gatherTicket(): ?Token
     {
-        // TODO: Implement gatherTicket() method.
+        if (!$this->request->hasHeader('Authorization')) {
+            return null;
+        }
+
+        $headerLines = $this->request->getHeader('Authorization');
+
+        foreach ($headerLines as $header) {
+            if (preg_match('/^Bearer\s(\S+)$/iuU', $header, $matches) !== 1) {
+                continue;
+            }
+
+            return new Token(value: $matches[1]);
+        }
+
+        return null;
     }
 
     public function storeTicket(TicketInterface $ticket): void
